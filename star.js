471fae7a3b950b4df9c76f2712f30eca9b11d3e5
Rouge <code@r0uge.org>
2018-09-14 02:09:51 -0700

    Add a thing

--- star.js
+++ star.js
@@ -5,23 +5,26 @@
 //	Define the starfield class.
 class Starfield {
     constructor() {
-        this.fps = 60;
         this.containerDiv = document.getElementById('back');
-        var canvas = document.createElement('canvas');
-        this.containerDiv.appendChild(canvas);
-        this.canvas = canvas;
+        this.canvas = document.createElement('canvas');
+        this.containerDiv.appendChild(this.canvas);
+        //	Get the drawing context.
+        this.ctx = this.canvas.getContext('2d');
         this.height = 0;
         this.width = 0;
         this.minVelocity = 15;
         this.maxVelocity = 30;
         this.stars = 100;
         this.old = 0;
+        let date = new Date();
+        let ddmm = String(date.getDate()) + String(date.getMonth + 1);
+        this.meme = (ddmm == "204");
     }
     //	The main function - initialises the starfield.
     initialise() {
         var self = this;
-        self.width = window.innerWidth;
-        self.height = window.innerHeight;
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
         window.onresize = () => {
             self.width = window.innerWidth;
             self.height = window.innerHeight;
@@ -34,15 +37,19 @@ class Starfield {
         //	Create the stars.
         var stars = [];
         for (var i = 0; i < this.stars; i++) {
-            stars[i] = new Star(Math.random() * this.width, Math.random() * this.height, Math.random() * 3 + 1, (Math.random() * this.maxVelocity) + this.minVelocity);
+            stars[i] = new Star(Math.random() * this.width, Math.random() * this.height, Math.floor(Math.random() * 7) + 10, (Math.random() * this.maxVelocity) + this.minVelocity);
         }
         this.stars = stars;
+        if(this.meme){
+            this.image = new Image();
+            this.image.src = "./Content/meme.png";
+        }
         //	Start the timer.
         requestAnimationFrame(this.raf.bind(this));
     }
     raf(time) {
         this.update(time);
-        this.draw()
+        (this.meme) ? this.memedraw() : this.draw();
         requestAnimationFrame(this.raf.bind(this));
     }
     update(t) {
@@ -53,25 +60,34 @@ class Starfield {
             star.y += dt * star.velocity;
             //	If the star has moved from the bottom of the screen, spawn it at the top.
             if (star.y > this.height) {
-                this.stars[i] = new Star(Math.random() * this.width, 0, Math.random() * 3 + 1, (Math.random() * this.maxVelocity) + this.minVelocity);
+                this.stars[i] = new Star(Math.random() * this.width, 0, Math.random() * 3 + 7, (Math.random() * this.maxVelocity) + this.minVelocity);
             }
         }
         this.old = t;
         return;
     }
     draw() {
-        //	Get the drawing context.
-        var ctx = this.canvas.getContext('2d');
         //	Draw the background.
-        ctx.fillStyle = '#000000';
-        ctx.fillRect(0, 0, this.width, this.height);
+        this.ctx.fillStyle = '#000000';
+        this.ctx.fillRect(0, 0, this.width, this.height);
         //	Draw stars.
-        ctx.fillStyle = '#ffffff';
+        this.ctx.fillStyle = '#ffffff';
         for (var i = 0; i < this.stars.length; i++) {
             var star = this.stars[i];
-            ctx.fillRect(star.x, star.y, star.size, star.size);
+            this.ctx.font = star.size + "px 'Roboto', sans-serif";
+            this.ctx.fillText("*", star.x, star.y);
         }
         return;
+    }
+    memedraw(){
+        //	Draw the background.
+        this.ctx.fillStyle = "#000000";
+        this.ctx.fillRect(0, 0, this.width, this.height);
+        //	Draw memes.
+        this.ctx.fillStyle = '#ffffff';
+        this.stars.forEach(star => {
+            this.ctx.drawImage(this.image, star.x, star.y, star.size * 4, star.size * 4)
+        });
     }
 }
 
